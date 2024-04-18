ea4b2e40ce46aa8a01c305224df33a9b208225c8
maharong <kija4545@naver.com>
2024-04-19 00:01:53 +0900

    BottomBar

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -3,13 +3,54 @@ import { useState } from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View,Button,Switch } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
 
-const MainScreen = ({ navigation }) => {
+const Tab = createBottomTabNavigator();
+
+const MainScreen = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="Home">
+                <Tab.Screen name="Home" component={HomeScreen} options={{tabBarIcon: () => <Entypo name="home" size={24} color="black"/>}}></Tab.Screen>
+                <Tab.Screen name="Calendar" component={CalendarScreen} options={{tabBarIcon: () => <Entypo name="calendar" size={24} color="black"/>}}></Tab.Screen>
+                <Tab.Screen name="Stat" component={StatScreen} options={{tabBarIcon: () => <Entypo name="bar-graph" size={24} color="black"/>}}></Tab.Screen>
+                <Tab.Screen name="Preferences" component={PreferencesScreen} options={{tabBarIcon: () => <Entypo name="cog" size={24} color="black"/>}}></Tab.Screen>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
+
+const HomeScreen = () => {
     return (
         <View>
-            <Text>메인화면</Text>
+            <Text>홈 화면</Text>
         </View>
     );
 };
 
-export default MainScreen
+const CalendarScreen = () => {
+    return (
+        <View>
+            <Text>캘린더 화면</Text>
+        </View>
+    );
+};
+
+const StatScreen = () => {
+    return (
+        <View>
+            <Text>근무 통계 화면</Text>
+        </View>
+    );
+};
+
+const PreferencesScreen = () => {
+    return (
+        <View>
+            <Text>환경 설정 화면</Text>
+        </View>
+    );
+};
+
+export { HomeScreen, CalendarScreen, StatScreen, PreferencesScreen };

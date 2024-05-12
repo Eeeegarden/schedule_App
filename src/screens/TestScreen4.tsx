6812ee34adefc4306907f4064a7daf5e6710e710
maharong <kija4545@naver.com>
2024-05-12 21:06:29 +0900

    Operator Main Screen, BottomBar

---
+++ src/screens/TestScreen4.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+
+const TestScreen4 = () => {
+    return (
+        <View>
+            <Text>하단 탭 테스트 페이지 4</Text>
+        </View>
+    );
+};
+
+export default TestScreen4;

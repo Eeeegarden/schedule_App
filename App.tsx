0a72669cb315b05e9a360eb972d165b16fad1ae6
Hoojin0 <jaajaa123@naver.com>
2024-05-29 14:56:10 +0900

    [ADD] ManagementAlba, WorkStat_A, WorkStat_M
    ManagementAlba: 알바생 관리(관리자)
    WorkStat_A: 근무통계표(알바생)
    WorkStat_M: 근무통계표(관리자)

--- App.tsx
+++ App.tsx
@@ -8,6 +8,9 @@ import SignupScreen from "./src/screens/SignupScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import FindpwScreen from "./src/screens/FindpwScreen";
 import MainNavigator from './src/screens/MainNavigator';
+import ManagementAlba from "./src/screens/ManagementAlba";
+import WorkStat_A from "./src/screens/WorkStat_A";
+import WorkStat_M from "./src/screens/WorkStat_M";
 
 const Stack = createNativeStackNavigator();
 
@@ -25,7 +28,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
+        {/* <Stack.Screen
           name="Auth"
           component={MyStack}
           options={{ headerShown: false }}
@@ -33,6 +36,11 @@ const App = () => {
         <Stack.Screen
           name="MainNavigator"
           component={MainNavigator}
+          options={{headerShown: false}}
+        /> */}
+        <Stack.Screen
+          name="test"
+          component={WorkStat_M}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
